6a472619a908acb97f4ff17da4316ecd37130e44
mikechavo <mchavez76@yahoo.com>
2023-09-13 09:35:14 -0700

    int config complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user1 = User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password")
+user2 = User.where(email: "test2@example.com").first_or_create(password: "password", password_confirmation: "password")
+
+user1_apartments = [ 
+  {
+    street: "Hibbard Lane",
+    unit: "20",
+    city: "Colorado Springs",
+    state: "CO",
+    square_footage: 1000,
+    price: "2000",
+    bedrooms: 5,
+    bathrooms: 2.5,
+    pets: "YES",
+    image:"https://www.apartments.com/blog/sites/default/files/styles/x_large_hq/public/image/2023-06/ParkLine-apartment-in-Miami-FL.jpg?itok=kQmw64UU"
+  }
+]
+
+user2_apartments = [ 
+  {
+    street: "Giant Lane",
+    unit: "290",
+    city: "Old Colorado City",
+    state: "CO",
+    square_footage: 1000,
+    price: "6000",
+    bedrooms: 2,
+    bathrooms: 2.5,
+    pets: "YES",
+    image:"https://www.apartments.com/blog/sites/default/files/styles/x_large_hq/public/image/2023-06/ParkLine-apartment-in-Miami-FL.jpg?itok=kQmw64UU"
+  }
+]
+
+user1_apartments.each do |apartment|
+  user1.apartments.create(apartment)
+  p "created: #{apartment}"
+end
+
+user2_apartments.each do |apartment|
+  user2.apartments.create(apartment)
+  p "created: #{apartment}"
+end
